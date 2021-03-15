16965ee82517cff8704ea25d2edfa1c14f753376
dsibagat <Damir.Sibagatulin@t-systems.com>
2021-03-15 16:58:05 +0300

    added files fo read me

--- src/test/java/tests/EmeaOpenPositionsTests.java
+++ src/test/java/tests/EmeaOpenPositionsTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static helpers.DriverHelper.getConsoleLogs;
